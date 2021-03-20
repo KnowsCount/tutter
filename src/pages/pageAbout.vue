2750564f805c371a01455ee201b7c7518e48bb44
KnowsCount <knowscount@gmail.com>
2021-03-20 11:11:55 +0800

    feat: tutt input box

--- src/pages/pageAbout.vue
+++ src/pages/pageAbout.vue
@@ -1,21 +1,23 @@
 <!--
  * @Date: 08/03/2021 07.37.12 +0800
  * @Author: KnowsCount
- * @LastEditTime: 13/03/2021 14.18.35 +0800
+ * @LastEditTime: 20/03/2021 10.33.13 +0800
  * @FilePath: /tutter/src/pages/pageAbout.vue
 -->
 <template>
-	<q-page>
-		<h5>About</h5>
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-			eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-			ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-			aliquip ex ea commodo consequat. Duis aute irure dolor in
-			reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-			pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-			culpa qui officia deserunt mollit anim id est laborum.
-		</p>
+	<q-page class="q-pa-lg">
+		<h4 class="q-mt-none q-mb-md text-weight-bold">About</h4>
+		<div class="text-body1">
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+				eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+				enim ad minim veniam, quis nostrud exercitation ullamco laboris
+				nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+				in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+				nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+				sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+		</div>
 	</q-page>
 </template>
 
--- src/pages/pageHome.vue
+++ src/pages/pageHome.vue
@@ -1,17 +1,73 @@
 <!--
  * @Date: 07/03/2021 08.58.13 +0800
  * @Author: KnowsCount
- * @LastEditTime: 08/03/2021 07.34.50 +0800
+ * @LastEditTime: 20/03/2021 11.09.37 +0800
  * @FilePath: /tutter/src/pages/pageHome.vue
 -->
 <template>
 	<q-page>
-		<h5>Home</h5>
+		<div class="q-py-lg q-px-md">
+			<q-input
+				bottom-slots
+				v-model="newTuttContent"
+				placeholder="What's Happening?"
+				counter
+				autogrow
+				maxlength="280"
+				:dense="dense"
+				class="tuttBox"
+			>
+				<template v-slot:before>
+					<q-avatar size="xl">
+						<img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+					</q-avatar>
+				</template>
+
+				<template v-slot:append>
+					<q-icon
+						v-if="text !== ''"
+						name="close"
+						@click="text = ''"
+						class="cursor-pointer"
+					/>
+					<q-icon name="schedule" />
+				</template>
+
+				<template v-slot:hint>
+					Field hint
+				</template>
+
+				<template v-slot:after>
+					<q-btn
+						:disable="!newTuttContent"
+						unelevated
+						rounded
+						nocaps
+						color="primary"
+						label="Tutt"
+					/>
+				</template>
+			</q-input>
+		</div>
 	</q-page>
 </template>
 
 <script>
 export default {
-	name: "pageHome"
+	name: "pageHome",
+	data() {
+		return {
+			newTuttContent: "",
+			ph: "",
+			dense: false
+		};
+	}
 };
 </script>
+
+<style>
+#tuttbox {
+	font-size: 19px;
+	line-height: 10.4 !important;
+}
+</style>
